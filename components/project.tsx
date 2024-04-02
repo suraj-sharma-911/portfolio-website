f05a1898d7e6d7342113824af343fd0974f83cba
suraj-sharma-911 <surajrs911@gmail.com>
2024-04-02 19:44:56 +0530

    bugfix project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -98,7 +98,7 @@ export default function Project({
           />
         </div>
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#686767] dark:bg-black/60 bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          {gitUrl && gitUrl !== "" ? (
+          {gitUrl ? (
             <Link
               href={gitUrl}
               className="h-14 w-14 mr-2 border-[0.15rem] relative rounded-full dark:border-[#ADB7BE] border-gray-400 hover:border-gray-950/40 dark:hover:border-gray-950/40 group/link"
@@ -107,7 +107,7 @@ export default function Project({
               <IoCodeSlashOutline className="h-10 w-10 dark:text-[#ADB7BE] text-gray-400 hover:text-gray-950/40 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-gray-950/40" />
             </Link>
           ) : null}
-          {previewUrl && previewUrl !== "" ? (
+          {previewUrl ? (
             <Link
               href={previewUrl}
               className="h-14 w-14 border-[0.15rem] relative rounded-full border-gray-400 hover:border-gray-950/40 dark:border-[#ADB7BE] dark:hover:border-gray-950/40 group/link"
