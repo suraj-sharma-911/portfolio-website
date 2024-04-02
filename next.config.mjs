ac9206957d5a1ebb57ead5fbb7555c221dc4039a
suraj-sharma-911 <surajrs911@gmail.com>
2024-04-02 14:26:49 +0530

    nextjs portfolio v1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+        serverComponentsExternalPackages: [
+            '@react-email/components',
+            '@react-email/render',
+            '@react-email/tailwind'
+        ]
+    }
+
+};
 
 export default nextConfig;
