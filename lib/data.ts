8e471d053b2560e21e8ed041377b5e94e36a3785
suraj-sharma-911 <surajrs911@gmail.com>
2024-04-02 19:34:04 +0530

    added portfolio image

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,5 @@
 import React from "react";
 import {CgWorkAlt} from "react-icons/cg";
-import {FaReact} from "react-icons/fa";
 import {LuGraduationCap} from "react-icons/lu";
 import amazonCloneProject from "@/public/amazonCloneProject.png";
 import portfolioImg from "@/public/portfolioImg.png";
